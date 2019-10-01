fe2fab24f0637b5914b7c005047f7aea5c164049
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-10-01 19:00:10 -0300

    Updates

---
+++ precache-manifest.6ea09a78296b795c1b37e261f24933cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3ab5cf069d6638fc9754596c1bafd0",
+    "url": "/entregadeepi/index.html"
+  },
+  {
+    "revision": "2e85f6278beba53a50cb",
+    "url": "/entregadeepi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "37aaa6588b9ba46fffaf",
+    "url": "/entregadeepi/static/js/2.58fab265.chunk.js"
+  },
+  {
+    "revision": "2e85f6278beba53a50cb",
+    "url": "/entregadeepi/static/js/main.483f1493.chunk.js"
+  },
+  {
+    "revision": "b923df2221ca8b0792bc",
+    "url": "/entregadeepi/static/js/runtime-main.eb61bc52.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/entregadeepi/static/media/logo.25bf045c.svg"
+  }
+]);
