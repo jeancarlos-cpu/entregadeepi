d5d0dfda9047e7448a971bdccb0ed6b821203676
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-12-04 15:30:27 -0300

    Updates

---
+++ precache-manifest.171fb06aa8b255342679656fe1580e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42bf41858cfebfc57adef3e97b4db13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7997da26788dd4f36d3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fa6ef0b27ef924088bd5",
+    "url": "/static/js/2.422c2ae9.chunk.js"
+  },
+  {
+    "revision": "d7997da26788dd4f36d3",
+    "url": "/static/js/main.56cf5c43.chunk.js"
+  },
+  {
+    "revision": "1560e401b547f7863bf7",
+    "url": "/static/js/runtime-main.117416a5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/entregadeepi/precache-manifest.429736beebb87b51444e61ba08814e87.js"
+  "/precache-manifest.171fb06aa8b255342679656fe1580e53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/entregadeepi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
